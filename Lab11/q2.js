7c5bfcc08ccfe5900fa3d40aa6f90ae26156f4d7
Thuong Nguyen <mttbit@gmail.com>
2022-07-05 14:37:29 -0500

    Add 3 solutionn for q2

--- Lab11/q2.js
+++ Lab11/q2.js
@@ -2,21 +2,48 @@
 
 const http = require("http");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 
 http
   .createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     if (url === "/") {
-      fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res);
-    } else if (url === "/file" && method === "GET") {
-      try {
-        const file = fs.readFileSync(path.join(__dirname, 'file.jpg'));
-        res.end(file);
-      } catch (error) {
-        res.end('Read file error!!!');
-      }
+      fs.createReadStream(path.join(__dirname, "index.html")).pipe(res);
+    } else if (url === "/file1" && method === "GET") {
+      solution1(req, res);
+    } else if (url === "/file2" && method === "GET") {
+      solution2(req, res);
+    } else if (url === "/file3" && method === "GET") {
+      solution3(req, res);
     }
   })
   .listen(4000);
+
+// Solution 1
+function solution1(req, res) {
+  try {
+    const file = fs.readFileSync(path.join(__dirname, "file.jpg"));
+    res.end(file);
+  } catch (error) {
+    res.end("Read file error!!!");
+  }
+}
+
+// Solution 2
+function solution2(req, res) {
+  fs.readFile(path.join(__dirname, "file.jpg"), (err, data) => {
+    if (err) throw err;
+    res.end(data);
+  });
+}
+
+// Solution 3
+function solution3(req, res) {
+  const file = fs.createReadStream(path.join(__dirname, "file.jpg"));
+  file.pipe(res);
+  file.on("error", (error) => {
+    console.log(error);
+    res.end("Read file error!!!");
+  });
+}
